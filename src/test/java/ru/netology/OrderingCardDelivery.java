c695a04809e3f20c63b166e742edce8cb86455e3
Татьяна Пислегина <ztv-1988@yandex.ru>
2022-10-07 09:15:36 +0500

    testDate

--- src/test/java/ru/netology/OrderingCardDelivery.java
+++ src/test/java/ru/netology/OrderingCardDelivery.java
@@ -1,13 +1,8 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selectors;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -18,23 +13,28 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 
-public class orderingCardDelivery {
+public class OrderingCardDelivery {
 
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+    }
     @Test
     void fullFilling() {
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue("Оренбург");
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        LocalDate date = LocalDate.now().plusDays(5);
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String datalocal = format.format(date);
-        $("[placeholder='Дата встречи']").setValue(datalocal);
+        String planningDate = generateDate(5);
+//        LocalDate date = LocalDate.now().plusDays(5);
+//        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+//        String datalocal = format.format(date);
+        $("[placeholder='Дата встречи']").setValue(planningDate);
         $("[name='name']").setValue("Мишинев Дмитрий");
         $("[name='phone']").setValue("+79235698562");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
         $("[data-test-id=notification]")
-                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + datalocal),
+                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + planningDate),
                         Duration.ofSeconds(15));
     }
 }
